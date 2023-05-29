c393f576ee72bb25585ef70c095f80eb414c53c9
ManuelF <manuel.floriani1999@gmail.com>
2023-05-29 16:03:51 +0200

    Esecuzione del ciclo v-for con push in array vuoto

--- main.js
+++ main.js
@@ -10,100 +10,19 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            mail1: '',
-            mail2: '',
-            mail3: '',
-            mail4: '',
-            mail5: '',
-            mail6: '',
-            mail7: '',
-            mail8: '',
-            mail9: '',
-            mail10: '',
+            email : [],
         }
     },
-
-    // il mounted è un evento che si scatena alla fine del 'montaggio' dell'applicazione, all'interno del quale noi passiamo ciò che l'app deve eseguire alla fine.
     mounted(){
+        // Ciclo per 10 volte il richiamo dell'API e pusho ogni volta il risultato nell'array vuoto
+       for(let i = 1; i <= 10; i++){
         axios
             .get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then(
                 risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail1 = risposta.data.response;
-            });
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail2 = risposta.data.response;
-            });
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail3 = risposta.data.response;
-            });
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail4 = risposta.data.response;
-            });
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail5 = risposta.data.response;
-            });
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail6 = risposta.data.response;
-            });
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail7 = risposta.data.response;
-            });
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail8 = risposta.data.response;
-            });
-            axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail9 = risposta.data.response;
-            });
-        axios
-            .get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then(
-                risposta => {
-                    // aggiungo il '.data' perchè all'interno del 'risposta' ci sono diverse informazioni , quelle che mi servono sono dentro il '.data'
-                    // const result = risposta.data.response;
-                    this.mail10 = risposta.data.response;
-            });
+                    this.email.push(risposta.data.response);
+                }
+            );
+        }
     }
 }).mount('#app')
